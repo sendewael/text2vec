d7d1e2cbb62b4a5dc3682b05fbc32c71c1377fd6
sendewael <r0856234@student.thomasmore.be>
2025-04-30 03:06:24 +0200

    text2vec update for plotting letter

--- shape_library.py
+++ shape_library.py
@@ -11,6 +11,20 @@ def get_shape_json(shape_name):
     elif shape_name == "triangle":
         x = [0, 1, -1, 0]
         y = [1, -1, -1, 1]
+    elif shape_name == "a":
+        x = [-1, 0, 1, 0.5, -0.5]
+        y = [-1, 1, -1, 0, 0]
+    elif shape_name == "b":
+        x = [-1, -1, 0.5, -1, 0.5, -1]
+        y = [1, -1, -0.5, 0, 0.5, 1]
+    elif shape_name == "c":
+        theta = np.linspace(np.pi / 4, 7 * np.pi / 4, 50)
+        x = np.cos(theta)
+        y = np.sin(theta)
+        x = np.insert(x, 0, [np.cos(np.pi / 4)])
+        y = np.insert(y, 0, [np.sin(np.pi / 4)])
+        x = np.append(x, np.cos(7 * np.pi / 4))
+        y = np.append(y, np.sin(7 * np.pi / 4))
     else:
         return None
 
